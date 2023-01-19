592a55bb222f9ff05aa6a64fcc7427bb9cd1fa6a
home-chef-app <home.chef.app.22@gmail.com>
2023-01-19 17:33:22 -0400

    Updating comments

--- src/services/apiBaseService.ts
+++ src/services/apiBaseService.ts
@@ -3,7 +3,7 @@ import config from '../config';
 const apiBaseUrl = `${config.API_BASE_URL}/${config.STAGE}/${config.API_VERSION}/`;
 
 // Helper class to make all requests to our api. We can customize as we find a cadence for how we hit our api.
-// No try/catch blocks is intentional, this way we can catch all errors in sagas and handle appropriately. Cormac, 2023-01-19
+// No try/catch blocks is intentional, this way we can catch all errors in redux and handle appropriately. Cormac, 2023-01-19
 export const get = async (path: string) => {
   console.log('GET: ', apiBaseUrl + path);
   const resp = await fetch(apiBaseUrl + path, {
@@ -71,6 +71,9 @@ export const del = async (path: string) => {
   return data;
 };
 
+// Helper function to handle what we return for different cases. This is where we could
+// even define our own error types to be thrown that could then invoke different actions
+// in our redux.
 const getReponseBody = (resp: any) => {
   switch (resp.status) {
     case 200: // ok
@@ -78,9 +81,9 @@ const getReponseBody = (resp: any) => {
       return resp.data;
     case 204:
       return true; // no content
-    case 404:
+    case 404: // this is where we could maybe throw different kinds of errors to be caught in our sagas
       throw new Error('Not found');
-    case 500: // this is where we could maybe throw different kinds of errors to be caught in our sagas
+    case 500:
       throw new Error('Server error');
     default:
       return resp.data;
